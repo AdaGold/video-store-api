dc84865f441f12a05c4661c570a8fbe129c7b7ef
Canaan West <canaan.west@gmail.com>
2017-11-06 14:35:32 -0800

    removed some spaces

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,5 @@
 Rails.application.routes.draw do
-  get 'movies', to: 'movies#index', as: 'movies'
-  get 'movies/:id', to: 'movies#show', as: 'movie'
-  post 'movie', to: 'movies#create', as: 'movie'
-  get 'customers', to: 'customers#index', as: 'customers'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/zomg', to: "application#zomg"
-
-
 
 end
