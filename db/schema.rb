b786c10253a023c649c1e34271d3733a69e1498b
Canaan West <canaan.west@gmail.com>
2017-11-06 12:53:59 -0800

    adding registered_at to customers model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171106204722) do
+ActiveRecord::Schema.define(version: 20171106205216) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20171106204722) do
     t.string "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "registered_at"
   end
 
   create_table "movies", force: :cascade do |t|
