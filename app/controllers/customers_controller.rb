ea90bcd345095ff0b1050f0c4890bdae0cf9b165
HelloMarisaMorris <hellomarisanmorris@gmail.com>
2017-11-06 14:54:27 -0800

    completed customer index tests

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,9 +1,11 @@
 class CustomersController < ApplicationController
+
   def index
     customers = Customer.all
 
     render(
-      json: customers.as_json(only: [:name, :address, :city, :state, :posta_code, :phone, :registered_at, :account_credit]), status: :ok
+      json: customers.as_json(only: [:name, :address, :city, :movies_checked_out_count, :state, :posta_code, :phone, :registered_at, :account_credit]), status: :ok
     )
   end
+
 end
--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -19,9 +19,38 @@ describe CustomersController do
   end
 
   it "returns an Array" do
-      get customers_path
+    get customers_path
 
-      body = JSON.parse(response.body)
-      body.must_be_kind_of Array
+    body = JSON.parse(response.body)
+    body.must_be_kind_of Array
+  end
+
+  it "returns all of the customers" do
+    get customers_path
+
+    body = JSON.parse(response.body)
+    body.length.must_equal Customer.count
+  end
+
+  it "returns customers with the exact required fields" do
+    keys = %w(account_credit address city movies_checked_out_count name phone registered_at state)
+
+    get customers_path
+    body = JSON.parse(response.body)
+
+    body.each do |customer|
+      customer.keys.sort.must_equal keys
     end
+  end
+
+  it "Returns an empty array if there are no customers " do
+    Customer.destroy_all
+
+    get customers_path
+
+    must_respond_with :success
+    body = JSON.parse(response.body)
+    body.must_be_kind_of Array
+    body.must_be :empty?
+  end
 end
