145d90740e5e332210c9ebddb98f13ba7db41de3
Canaan West <canaan.west@gmail.com>
2017-11-06 13:12:30 -0800

    adding testing vadidations for customers

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -3,8 +3,15 @@ require 'test_helper'
 class CustomerTest < ActiveSupport::TestCase
   describe Customer do
     let(:customer) {customers(:one)}
+
     it "must be valid" do
       customer.must_be :valid?
+    end
+
+    it "won't be valid without a name" do
+      customer.must_be :valid?
+      customer.name = nil
+      customer.wont_be :valid?
     end
   end
 end
