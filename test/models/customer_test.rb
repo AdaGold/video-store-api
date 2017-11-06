ecf68acd39ad8997fef3781c636d9d2388e07a23
Canaan West <canaan.west@gmail.com>
2017-11-06 13:10:36 -0800

    updating test helper

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
-
+#
 class CustomerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  describe Customer do
+    let(:customer) {customers(:one)}
+    it "must be valid" do
+      customer.must_be :valid?
+    end
+  end
 end
