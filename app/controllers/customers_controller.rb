1481778b52c4c1fac90874d41034d4b4029394ac
HelloMarisaMorris <hellomarisanmorris@gmail.com>
2017-11-06 14:34:19 -0800

    migration for customers and model tests

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,4 +1,9 @@
 class CustomersController < ApplicationController
   def index
+    customers = Customer.all
+
+    render(
+      json: customers.as_json(only: [:name, :address, :city, :state, :posta_code, :phone, :registered_at, :account_credit]), status: :ok
+    )
   end
 end
