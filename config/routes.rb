1183bb737ae38749efe5a28825c9e910ac2937f3
Canaan West <canaan.west@gmail.com>
2017-11-06 15:56:32 -0800

    modifying show route for movie:

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   get 'movies', to: 'movies#index', as: 'movies'
-  get 'movies/:id', to: 'movies#show', as: 'movie'
+  get 'movie/:id', to: 'movies#show', as: 'movie'
   post 'movie', to: 'movies#create', as: 'create_movie'
   get 'customers', to: 'customers#index', as: 'customers'
 
