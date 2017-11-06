b21bea6f49c3de8aa921c0bafbb51ea546489f66
Canaan West <canaan.west@gmail.com>
2017-11-06 14:49:06 -0800

    movies controller tests for index

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,10 @@
 class MoviesController < ApplicationController
   def index
+    movies = Movie.all
+  render(
+      json: movies.as_json(only: [:title, :overview, :release_date, :inventory]), status: :ok
+    )
+
   end
 
   def show
