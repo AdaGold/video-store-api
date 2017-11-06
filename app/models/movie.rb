d060dc59261effcd37e90aafdaf0e3804d527656
Canaan West <canaan.west@gmail.com>
2017-11-06 13:36:37 -0800

    adding numericality validation to movie inventory

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,4 @@
 class Movie < ApplicationRecord
   validates :title, presence: true
-  validates :inventory, presence: true 
+  validates :inventory, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0}
 end
