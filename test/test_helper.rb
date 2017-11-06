efcb97bd48699ebc4e74846c74b80457c51a9095
Canaan West <canaan.west@gmail.com>
2017-11-06 15:29:29 -0800

    adding coverage to gitignore

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,10 @@
+require 'simplecov'
+SimpleCov.start
 ENV["RAILS_ENV"] = "test"
 require File.expand_path("../../config/environment", __FILE__)
 require "rails/test_help"
 require "minitest/rails"
+
 
 Minitest::Reporters.use!(
   Minitest::Reporters::SpecReporter.new,
