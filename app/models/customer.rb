1201bc2f3c0cdc40deab7bafddb3351024710ede
Canaan West <canaan.west@gmail.com>
2017-11-06 13:01:44 -0800

    added validations to customer and movie

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,3 @@
 class Customer < ApplicationRecord
-  
+  validates :name, presence: true
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,4 @@
 class Movie < ApplicationRecord
+  validates :title, presence: true
+  validates :inventory, presence: true 
 end
