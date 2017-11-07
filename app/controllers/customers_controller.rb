787ae54c0e339dc22334f5086f6d61a8d32acda1
HelloMarisaMorris <hellomarisanmorris@gmail.com>
2017-11-06 16:37:42 -0800

    fixed typ0

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -3,9 +3,8 @@ class CustomersController < ApplicationController
   def index
     customers = Customer.all
 
-    render(
-      json: customers.as_json(only: [:name, :address, :city, :movies_checked_out_count, :state, :posta_code, :phone, :registered_at, :account_credit]), status: :ok
-    )
+    render json: customers.as_json(only: [:id, :name, :address, :city, :movies_checked_out_count, :state, :postal_code, :phone, :registered_at, :account_credit]), status: :ok
+
   end
 
 end
