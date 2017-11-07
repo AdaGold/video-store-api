0ba8907500e35d9c355fc34071fe9d5bfc3a4c2e
HelloMarisaMorris <hellomarisanmorris@gmail.com>
2017-11-07 11:19:48 -0800

    added test questions for rental controller

--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -1,7 +1,56 @@
 require 'test_helper'
 
 class RentalsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  describe "Checkout" do
+    it "Should create an instance of rental" do
+
+    end
+
+    it "Must have a movie and customer ID" do
+
+    end
+
+    it "Checkout must be todays date" do
+
+    end
+
+    it "Duedate should be 7 days from checkout date" do
+
+    end
+
+    it "Should decrease available inventory when a movie is checked out" do
+
+    end
+
+  end
+
+  describe "Checkin" do
+    it "Should require a user ID " do
+
+    end
+
+    it "Should have a movie ID" do
+
+    end
+
+    it "Should delete an instance of rental" do
+
+    end
+
+    it "Available inventory should increase once movie is checked in" do
+
+    end
+  end
+
+  describe "Overdue" do
+    it "Should return a list of Overdue rentals as of todays date" do
+
+    end
+
+    it "Cannot display are list of movies that are not yet overdue" do
+
+    end
+  end
+
 end
