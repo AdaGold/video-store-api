7ee1cb8554fbe0374d1cb4ccbe9ba2c0bc7679b2
Canaan West <canaan.west@gmail.com>
2017-11-07 09:53:23 -0800

    customer serializer

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,10 +2,7 @@ class CustomersController < ApplicationController
 
   def index
     customers = Customer.all
-
-
-    render json: customers.as_json(only: [:id, :name, :address, :city, :movies_checked_out_count, :state, :postal_code, :phone, :registered_at, :account_credit]), status: :ok
-
+    render json: customers, status: :ok
   end
 
 end
