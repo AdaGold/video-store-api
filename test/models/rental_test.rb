a239c65c3824eb7428fa78f02010acf476376ee1
Canaan West <canaan.west@gmail.com>
2017-11-12 15:27:14 -0800

    missing some tests for rental?

--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class RentalTest < ActiveSupport::TestCase
-  describe "" do
-    
+  describe "Rental" do
+    it "is valid when given valid input" do
+      expected_count = Rental.count + 1
+      Rental.create(customer_id: Customer.first.id, movie_id: Movie.first.id)
+      Rental.count.must_equal expected_count
+    end
+
+    it "is invalid without customer_id and movie_id" do
+        expected_count = Rental.count
+        Rental.create(customer_id: nil, movie_id: Movie.first.id)
+        Rental.count.must_equal expected_count
+    end
+
   end
 end
