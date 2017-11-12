7c79dd9809e2bbb82e34f2352d0215e7390ce12e
Canaan West <canaan.west@gmail.com>
2017-11-12 14:22:35 -0800

    adding dedicated sort_by method to customers controller

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,6 +1,11 @@
 class CustomersController < ApplicationController
 
   def index
+    customers = Customer.all
+    render json: customers, status: :ok
+  end
+
+  def sort_by
     begin
       customers = Customer.sort_by(params[:sort])
         # render json: params
