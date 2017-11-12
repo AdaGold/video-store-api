26e0f4bb72cc0c7fc5f7f3b97efd4f77a4b19cd1
Canaan West <canaan.west@gmail.com>
2017-11-12 12:48:48 -0800

    connecting params to index function

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,9 +2,10 @@ class CustomersController < ApplicationController
 
   def index
     begin
-      customers = Customer.sort_by("girl")
+      customers = Customer.sort_by(params[:sort])
+        # render json: params
         render json: customers, status: :ok
-    rescue
+    rescue ArgumentError
        render status: :bad_request
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  get 'movies', to: 'movies#index', as: 'movies'
+  # get 'movies', to: 'movies#index', as: 'movies'
   get 'movies/:id', to: 'movies#show', as: 'movie'
   post 'movies', to: 'movies#create', as: 'create_movie'
   get 'customers', to: 'customers#index', as: 'customers'
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   post 'rentals/checkin', to: 'rentals#checkin', as: 'checkin'
   get 'rentals/overdue', to: 'rentals#overdue', as: 'overdue'
 
-  get '/customers?sort=:field&n=:perpage&p=:page', to: 'customers#sort_by', as:'sort_by'
+  get "/customers?sort=:field", to: 'customers#index', as:'index'
 
   get '/zomg', to: "application#zomg"
 
