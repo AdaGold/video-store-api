f6923794cb936914177e013bfea7bab2d1c01f7a
dee del rosario <dee@adadevelopersacademy.org>
2020-05-25 16:34:15 -0700

    renames movie model to videos

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,10 @@ JSON.parse(File.read('db/seeds/customers.json')).each do |customer|
   Customer.create!(customer)
 end
 
-JSON.parse(File.read('db/seeds/movies.json')).each do |movie|
-  Movie.create!(movie)
+puts "The database now has #{Customer.count} customers"
+
+JSON.parse(File.read('db/seeds/videos.json')).each do |video|
+  Video.create!(video)
 end
+
+puts "The database now has #{Video.count} videos"
