fd795469e0ba5c0776eea3932d3dcc62b8be054d
CheezItMan <chris@adadevelopersacademy.org>
2020-11-23 13:00:33 -0800

    video create action tests

--- test/controllers/videos_controller_test.rb
+++ test/controllers/videos_controller_test.rb
@@ -83,11 +83,35 @@ describe VideosController do
 
       # Assert
       expect {
-        post video_path, params: video_hash
+        post videos_path, params: video_hash
       }.must_change "Video.count", 1
 
       must_respond_with :created
     end
-  end
 
+    it "will respond with bad request and errors for an invalid movie" do
+      # Arrange
+      video_hash = {
+        title: "Alf the movie",
+        overview: "The most early 90s movie of all time",
+        release_date: "December 16th 2025",
+        total_inventory: 6,
+        available_inventory: 6
+      }
+  
+      video_hash[:title] = nil
+  
+      # Assert
+      expect {
+        post videos_path, params: video_hash
+      }.wont_change "Video.count"
+      body = JSON.parse(response.body)
+
+      expect(body.keys).must_include "errors"
+      expect(body["errors"].keys).must_include "title"
+      expect(body["errors"]["title"]).must_include "can't be blank"
+  
+      must_respond_with :bad_request
+    end
+  end
 end
